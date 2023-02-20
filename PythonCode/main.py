c484351eab4f295f74c454d76771d0910bccce71
ghbakker <g.h.bakker@st.hanze.nl>
2023-02-20 15:19:47 +0100

    Created a python project within the repository

---
+++ PythonCode/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""
+Main.py: main.py is used to handle the user input, call the right functions and return the final output
+"""
+
+__author__ = "Gijs Bakker"
+__version__ = 0.1
