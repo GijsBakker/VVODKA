255c5ebff3367a0b10ff0a2da59dabc9c240fe33
ghbakker <g.h.bakker@st.hanze.nl>
2023-02-21 14:31:43 +0100

    Removed unnecessary comments

--- PythonCode/TestCode/K-merTest.py
+++ PythonCode/TestCode/K-merTest.py
@@ -3,19 +3,16 @@
 """
 K-merTest: is used to test the functions in the KMer script
 """
-# unit test case
 import unittest
 from Scripts import KMer
 
 
 class TestKmer(unittest.TestCase):
-    # test function to test equality of two value
     def test_find_kmer(self):
         kmer = "ATC"
         sequence = "ATCGGGTATC"
         expected = [0, 7]
         got = KMer.find_kmer(kmer, sequence)
-        # assertEqual() to check equality of first & second value
         self.assertEqual(expected, got)
 
     def test_sequence_to_kmer(self):
@@ -23,7 +20,6 @@ class TestKmer(unittest.TestCase):
         sequence = "ATCGT"
         expected = ["ATC", "TCG", "CGT"]
         got = KMer.sequence_to_kmer(sequence, size)
-        # assertEqual() to check equality of first & second value
         self.assertEqual(expected, got)
 
     def test_sequence_to_kmer_small(self):
@@ -31,7 +27,6 @@ class TestKmer(unittest.TestCase):
         sequence = "ATCGT"
         expected = ["A", "T", "C", "G", "T"]
         got = KMer.sequence_to_kmer(sequence, size)
-        # assertEqual() to check equality of first & second value
         self.assertEqual(expected, got)
 
     def test_find_overlapping_kmers(self):
@@ -40,7 +35,6 @@ class TestKmer(unittest.TestCase):
         sequence_two = "GATCG"
         expected = [[0, 1], [1, 2]]
         got = KMer.find_overlapping_kmers(sequence, sequence_two, size)
-        # assertEqual() to check equality of first & second value
         self.assertEqual(expected, got)
 
     def test_find_multiple_overlapping_kmers(self):
@@ -49,7 +43,6 @@ class TestKmer(unittest.TestCase):
         sequence_two = "GATCGATC"
         expected = [[0, 0, 1], [1, 5, 2]]
         got = KMer.find_overlapping_kmers(sequence, sequence_two, size)
-        # assertEqual() to check equality of first & second value
         self.assertEqual(expected, got)
 
 
