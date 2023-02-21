3d95ce19ccf2ac75f0dac2c8b43ffec3cfeb5d1e
ghbakker <g.h.bakker@st.hanze.nl>
2023-02-21 11:58:53 +0100

    Added function to split sequence into Kmers

--- PythonCode/Scripts/KMer.py
+++ PythonCode/Scripts/KMer.py
@@ -8,8 +8,12 @@ KMer.py: Is used to find k-mers in genomes
 def find_kmer(k_mer, sequence):
     """
     This function compare looks for a k-mer within a sequence
+    :parallel: This function can be run next to other functions with different k_mers
+    :param k_mer: a k_mer string
+    :param sequence: a sequence string
     :return: a list of the positions of the found kmers
     """
+
     # TODO determine where the position is marked. (The first of the kmer or in the middle. Even number can be a
     #  problem)
     kmer_len = len(k_mer)
@@ -18,3 +22,18 @@ def find_kmer(k_mer, sequence):
         if sequence[pos:pos+kmer_len] == k_mer:
             positions.append(pos)
     return positions
+
+
+def sequence_to_kmer(sequence, kmer_size):
+    """
+    This function splits a sequence into k_mers
+    :parallel: This function is necessary for a parallel program. This function wil be run so after each thread can have
+    its own k_mers. This function is not necessary when not run in parallel.
+    :param sequence: A string of the sequence
+    :param kmer_size: The size of the k_mers. Has to be more than 0 and not larger than the sequence
+    :return: A list of all the K_mers
+    """
+    kmers = []
+    for start in range(len(sequence)-kmer_size+1):
+        kmers.append(sequence[start:start+kmer_size])
+    return kmers
--- PythonCode/TestCode/K-merTest.py
+++ PythonCode/TestCode/K-merTest.py
@@ -10,11 +10,27 @@ from Scripts import KMer
 
 class TestKmer(unittest.TestCase):
     # test function to test equality of two value
-    def test_kmer(self):
+    def test_find_kmer(self):
         kmer = "ATC"
         sequence = "ATCGGGTATC"
         expected = [0, 7]
         got = KMer.find_kmer(kmer, sequence)
+        # assertEqual() to check equality of first & second value
+        self.assertEqual(expected, got)
+
+    def test_sequence_to_kmer(self):
+        size = 3
+        sequence = "ATCGT"
+        expected = ["ATC", "TCG", "CGT"]
+        got = KMer.sequence_to_kmer(sequence, size)
+        # assertEqual() to check equality of first & second value
+        self.assertEqual(expected, got)
+
+    def test_sequence_to_kmer_small(self):
+        size = 1
+        sequence = "ATCGT"
+        expected = ["A", "T", "C", "G", "T"]
+        got = KMer.sequence_to_kmer(sequence, size)
         # assertEqual() to check equality of first & second value
         self.assertEqual(expected, got)
 
